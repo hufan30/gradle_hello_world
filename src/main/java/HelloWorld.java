5ab97842ecf7db225321223e7fe6baeabc6f91f7
hufan <hufan30@163.com>
2021-11-16 23:26:39 +0800

    Create HelloWorld.java

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author hufan
+ * @date 2021/11/16 11:22 下午
+ * @annotation
+ */
+public class HelloWorld {
+    public void test(){
+        StringUtils.isEmpty("string");
+    }
+}
